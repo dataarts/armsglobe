62dc4871ea52703b60935de1b749f86b1e36e179
Dan Petker <dpetker@users.noreply.github.com>
2015-02-02 15:16:27 -0500

    Reduced the arc in the line visualization.

--- js/visualize_lines.js
+++ js/visualize_lines.js
@@ -7,30 +7,30 @@ function makeConnectionLineGeometry( exporter, importer, value, type ){
 
 	// console.log("making connection between " + exporter.countryName + " and " + importer.countryName + " with code " + type );
 
-	var distanceBetweenCountryCenter = exporter.center.clone().subSelf(importer.center).length();		
+	var distanceBetweenCountryCenter = exporter.center.clone().subSelf(importer.center).length();
 
 	//	how high we want to shoot the curve upwards
-	var anchorHeight = globeRadius + distanceBetweenCountryCenter * 0.7;
+	var anchorHeight = globeRadius + distanceBetweenCountryCenter * 0.3;
 
 	//	start of the line
 	var start = exporter.center;
 
 	//	end of the line
 	var end = importer.center;
-	
+
 	//	midpoint for the curve
-	var mid = start.clone().lerpSelf(end,0.5);		
+	var mid = start.clone().lerpSelf(end,0.5);
 	var midLength = mid.length()
 	mid.normalize();
-	mid.multiplyScalar( midLength + distanceBetweenCountryCenter * 0.7 );			
+	mid.multiplyScalar( midLength + distanceBetweenCountryCenter * 0.4 );
 
 	//	the normal from start to end
 	var normal = (new THREE.Vector3()).sub(start,end);
 	normal.normalize();
 
-	/*				     
+	/*
 				The curve looks like this:
-				
+
 				midStartAnchor---- mid ----- midEndAnchor
 			  /											  \
 			 /											   \
@@ -43,19 +43,19 @@ function makeConnectionLineGeometry( exporter, importer, value, type ){
 	var distanceHalf = distanceBetweenCountryCenter * 0.5;
 
 	var startAnchor = start;
-	var midStartAnchor = mid.clone().addSelf( normal.clone().multiplyScalar( distanceHalf ) );					
+	var midStartAnchor = mid.clone().addSelf( normal.clone().multiplyScalar( distanceHalf ) );
 	var midEndAnchor = mid.clone().addSelf( normal.clone().multiplyScalar( -distanceHalf ) );
 	var endAnchor = end;
 
 	//	now make a bezier curve out of the above like so in the diagram
-	var splineCurveA = new THREE.CubicBezierCurve3( start, startAnchor, midStartAnchor, mid);											
+	var splineCurveA = new THREE.CubicBezierCurve3( start, startAnchor, midStartAnchor, mid);
 	// splineCurveA.updateArcLengths();
 
 	var splineCurveB = new THREE.CubicBezierCurve3( mid, midEndAnchor, endAnchor, end);
 	// splineCurveB.updateArcLengths();
 
 	//	how many vertices do we want on this guy? this is for *each* side
-	var vertexCountDesired = Math.floor( /*splineCurveA.getLength()*/ distanceBetweenCountryCenter * 0.02 + 6 ) * 2;	
+	var vertexCountDesired = Math.floor( /*splineCurveA.getLength()*/ distanceBetweenCountryCenter * 0.02 + 6 ) * 2;
 
 	//	collect the vertices
 	var points = splineCurveA.getPoints( vertexCountDesired );
@@ -70,7 +70,7 @@ function makeConnectionLineGeometry( exporter, importer, value, type ){
 	points.push( vec3_origin );
 
 	var val = value * 0.0003;
-	
+
 	var size = (10 + Math.sqrt(val));
 	size = constrain(size,0.1, 60);
 
