5fe45604bea168eb3842183e039b58f0e37d8ad5
Dan Petker <dan.petker@esentire.com>
2015-02-19 15:41:28 -0500

    Minor refactoring to eventually support data using arbitrary lat/long.

--- js/geopins.js
+++ js/geopins.js
@@ -1,43 +1,48 @@
 function loadGeoData( latlonData ){
     //	-----------------------------------------------------------------------------
-    //	Load the world geo data json, per country	
-
-	var sphereRad = 1;				
-	var rad = 100;
+    //	Load the world geo data json, per country
 
 	//	iterate through each set of country pins
-	for ( var i in latlonData.countries ) {										
-		var country = latlonData.countries[i];	
-		
+	for ( var i in latlonData.countries ) {
+		var country = latlonData.countries[i];
+
 		//	can we even find the country in the list?
 		// if( countryLookup[country.n.toUpperCase()] === undefined ){
 		// 	console.log('could not find country that has country code: ' + country.n)
-		// 	continue;				
+		// 	continue;
 		// }
 
 		//	save out country name and code info
 		country.countryCode = i;
-		country.countryName = countryLookup[i];			
+		country.countryName = countryLookup[i];
 
 		//	take the lat lon from the data and convert this to 3d globe space
-        var lon = country.lon - 90;
-        var lat = country.lat;
-        
-        var phi = Math.PI/2 - lat * Math.PI / 180 - Math.PI * 0.01;
-        var theta = 2 * Math.PI - lon * Math.PI / 180 + Math.PI * 0.06;
-		
-		var center = new THREE.Vector3();                
-        center.x = Math.sin(phi) * Math.cos(theta) * rad;
-        center.y = Math.cos(phi) * rad;
-        center.z = Math.sin(phi) * Math.sin(theta) * rad;  	
-	
-		//	save and catalogue       
+		var center = convertLatLonToVector( country.lat, country.lon );
+
+		//	save and catalogue
 		country.center = center;
-		countryData[country.countryName] = country;	
-	}		
+		countryData[country.countryName] = country;
+	}
 
 	// console.log(countryData);
-}					
+}
+
+// Convert arbitrary lat/lon to a vector within the globe's space
+function convertLatLonToVector( lat, lon ) {
+	var rad = 100;
+
+	lon -= 90;
+
+	var phi = Math.PI/2 - lat * Math.PI / 180 - Math.PI * 0.01;
+	var theta = 2 * Math.PI - lon * Math.PI / 180 + Math.PI * 0.06;
+
+	var vec = new THREE.Vector3();
+	vec.x = Math.sin(phi) * Math.cos(theta) * rad;
+	vec.y = Math.cos(phi) * rad;
+	vec.z = Math.sin(phi) * Math.sin(theta) * rad;
+
+	return vec;
+}
 
 //	convenience function to get the country object by name
 function getCountry(name){
